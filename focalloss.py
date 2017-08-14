7258b782a7cfd2b9015f0dd562d1d4705c783847
clcarwin <clcarwin@gmail.com>
2017-08-14 16:55:57 +0800

    speedup and add test

--- focalloss.py
+++ focalloss.py
@@ -3,16 +3,21 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
 
-
 class FocalLoss(nn.Module):
     def __init__(self, gamma=0):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
 
     def forward(self, input, target):
+        if input.dim()>2:
+            input = input.view(input.size(0),input.size(1),-1)  # N,C,H,W => N,C,H*W
+            input = input.transpose(1,2)    # N,C,H*W => N,H*W,C
+            input = input.contiguous().view(-1,input.size(2))   # N,H*W,C => N*H*W,C
+        target = target.view(-1,1)
+
         logpt = F.log_softmax(input)
+        logpt = logpt.gather(1,target)
         pt = Variable(logpt.data.exp())
 
         loss = -1 * (1-pt)**self.gamma * logpt
-        loss = loss.index_select(1,target).diag(0).mean()
-        return loss
+        return loss.mean()
---
+++ focalloss_test.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import os,sys,random,time
+import argparse
+
+from focalloss import *
+
+
+
+start_time = time.time()
+for i in range(1000):
+    x = torch.rand(12800,2)*random.randint(1,10)
+    x = Variable(x.cuda())
+    l = torch.rand(12800).ge(0.1).long()
+    l = Variable(l.cuda())
+
+    output0 = FocalLoss(gamma=0)(x,l)
+    output1 = nn.CrossEntropyLoss()(x,l)
+    a = output0.data[0]
+    b = output1.data[0]
+    if abs(a-b)>1e-5: print(a-b,a,b)
+print('time:',time.time()-start_time)
+
+
+
+
+
+start_time = time.time()
+for i in range(100):
+    x = torch.rand(128,1000,8,4)*random.randint(1,10)
+    x = Variable(x.cuda())
+    l = torch.rand(128,8,4)*1000    # 1000 is classes_num
+    l = l.long()
+    l = Variable(l.cuda())
+
+    output0 = FocalLoss(gamma=0)(x,l)
+    output1 = nn.NLLLoss2d()(F.log_softmax(x),l)
+    a = output0.data[0]
+    b = output1.data[0]
+    if abs(a-b)>1e-5: print(a-b,a,b)
+print('time:',time.time()-start_time)
